998660b0e45024dc5b3da98f9eb319283379a961
Shashank Sharma <shashanksharma.er104@gmail.com>
2024-02-15 20:34:17 +0530

    Added screenshot capability in hooks.ts file

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,19 +1,34 @@
-import { Before, After } from "@cucumber/cucumber";
-import { Browser, Page, chromium } from "@playwright/test";
+import { Before, After, BeforeAll, AfterAll, Status } from "@cucumber/cucumber";
+import { Browser, BrowserContext, Page, chromium } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let browser: Browser;
-let page: Page;
+let context: BrowserContext;
 
-Before(async function() {
-    browser = await chromium.launch({
-        headless: false
-      });
-      page = await browser.newPage();
-      pageFixture.page = page;
+BeforeAll(async function() {
+    browser = await chromium.launch({ headless: false});
 });
 
-After(async function() {
+Before(async function() {
+    context = await browser.newContext();
+    const page = await context.newPage();
+    pageFixture.page = page;
+});
+
+After(async function({ pickle, result }) {
+    let img: Buffer;
+    console.log("Scenario status: " + pickle.name + " ==> " + result?.status);
+    // take screenshot
+    if(result?.status == Status.PASSED) {
+        img = await pageFixture.page.screenshot({
+            path: `./test-result/screenshots/${pickle.name}.png`, type: "png"
+        });
+        this.attach(img, "image/png");
+    }
     await pageFixture.page.close();
+    await context.close();
+});
+
+AfterAll(async function() {
     await browser.close();
 });
